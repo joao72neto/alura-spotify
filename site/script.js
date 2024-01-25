6a08aef53c74ca890206e167645e6543b07ea103
João Salvador Neto <99670872+joao72neto@users.noreply.github.com>
2024-01-25 18:51:55 -0300

    Pesquisa completa

--- site/script.js
+++ site/script.js
@@ -2,26 +2,30 @@ const searchInput = document.getElementById('search_input')
 const resultArtist = document.getElementById('result-artist')
 const resultPlaylist = document.getElementById('result-playlists')
 
-function requestApi(searchTerm){
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((reponse) => reponse.json())
-        //.then((result) => displayResults(result))
-}
+function requestApi(searchTerm) {
+    fetch(`http://localhost:3000/artists`)
+      .then((response) => response.json())
+      .then((results) => {
+        const filteredResults = results.filter((artist) =>
+          artist.name.toLowerCase().includes(searchTerm)
+        );
+        displayResults(filteredResults);
+      });
+  }
 
-/*function displayResults(result){
+function displayResults(result){
     resultPlaylist.classList.add('hidden')
     const artistName = document.getElementById('artist-name')
     const artistImage = document.getElementById('artist-img')
 
     result.forEach(element => {
         artistName.innerText = element.name
-        artistImage.src.element.urlImg
+        artistImage.src = element.urlImg
     });
 
     resultArtist.classList.remove('hidden')
 
-}*/
+}
 
 document.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase()
